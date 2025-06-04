2ba7b5c85ac274c4cbe9bf6753dfeeebd649acd9
josiel <josielsoares_2@hotmail.com>
2025-06-03 20:23:17 -0400

    fix:adicionado anotação faltante

--- src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -3,10 +3,12 @@ package com.devsuperior.dslist.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.repositories.GameRepository;
 
+@Service
 public class GameListService {
 
     @Autowired
